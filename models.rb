79e305e4ee8dd48f9eab4106c51825ba8b69940a
Carl Hörberg <carl.hoerberg@gmail.com>
2012-10-18 09:43:20 +0800

    CloudControl credentials parsing

--- models.rb
+++ models.rb
@@ -1,6 +1,11 @@
 require 'sequel'
 
-DB = Sequel.connect ENV['ELEPHANTSQL_URL'] || 'postgres://localhost/rubypgexample'
+if ENV['CRED_FILE']
+  creds = JSON.parse ENV['CRED_FILE']
+  DB = Sequel.connect creds['ELEPHANTSQL']['ELEPHANTSQL_URL']
+else
+  DB = Sequel.connect ENV['ELEPHANTSQL_URL'] || 'postgres://localhost/rubypgexample'
+end
 
 class Todo < Sequel::Model
 end
